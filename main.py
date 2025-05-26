99f24e9d6df63dbb4295ed11cba9372245916d07
MacBook <macbook@MacBooks-MacBook-Pro-2.local>
2025-05-26 18:19:47 +0700

    up

--- main.py
+++ main.py
@@ -16,6 +16,11 @@ class Item(BaseModel):
      price: float
      age:int
      mes:Optional[str]=None
+class UpdateItem(BaseModel):
+     name:Optional[str]=None
+     price:Optional[float]=None
+     age:Optional[int]=None
+     mes:Optional[str]=None
 data={
     "1":Item(name="item1",price=10.5,age=20),
 }
@@ -50,3 +55,28 @@ def create_user(user_id:int,item:Item):
     data[user_id]=item
 # {'name':item.name,'price':item.price,'age':item.age,'mse':item.mes}
     return {"message": "User created successfully", "user": data[user_id]}
+# update 
+@app.put("/users-update/{user_id}")
+def update_user(user_id: int, item:UpdateItem):
+    if user_id not in data:
+        return {"message": "User not found"}
+    # update data
+    # data[user_id] = item
+    if item.name !=None:
+        data[user_id].name=item.name
+    if item.price !=None:
+        data[user_id].price=item.price
+    if item.age !=None:
+        data[user_id].age=item.age
+    if item.mes !=None:
+        data[user_id].mes=item.mes
+
+    return {"message": "User updated successfully", "user": data[user_id]}
+#  delect 
+@app.delete("/user-delete")
+def delete_user(user_id: int=Query(...,description="input your id you wnat delete ")):
+    if user_id not in data:
+        return {"message": "User not found"}
+    # delete data
+    del data[user_id]
+    return {"message": "User deleted successfully"}
